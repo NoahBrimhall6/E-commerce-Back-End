50d37bd06ef2624c25d8c8ea793666c5ff81bbfa
Noah Brimhall <noahbrimhall6@gmail.com>
2022-11-22 13:47:41 -0700

    Created CRUD routes for category-routes.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,38 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
 // Categories have many Products
-
 // Products belongToMany Tags (through ProductTag)
-
 // Tags belongToMany Products (through ProductTag)
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
+
+Product.hasMany(ProductTag, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE'
+});
+
+Tag.hasMany(ProductTag, {
+  foreignKey: 'tag_id',
+  onDelete: 'CASCADE'
+});
+
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
+
+ProductTag.belongsTo(Product, {
+  foreignKey: 'product_id'
+});
+
+ProductTag.belongsTo(Tag, {
+  foreignKey: 'tag_id'
+});
+
+
 
 module.exports = {
   Product,
