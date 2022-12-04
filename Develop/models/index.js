3095401a27e6ee495bbfd6c644971af0acd33602
Noah Brimhall <noahbrimhall6@gmail.com>
2022-12-03 17:12:28 -0700

    Updated product routes.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -6,27 +6,27 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 // Categories have many Products
-// Products belongToMany Tags (through ProductTag)
-// Tags belongToMany Products (through ProductTag)
 
 Category.hasMany(Product, {
   foreignKey: 'category_id',
   onDelete: 'CASCADE'
 });
 
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
+
+// Products belongToMany Tags (through ProductTag)
+// Tags belongToMany Products (through ProductTag)
+
 Product.hasMany(ProductTag, {
   foreignKey: 'product_id',
-  onDelete: 'CASCADE'
 });
 
 Tag.hasMany(ProductTag, {
   foreignKey: 'tag_id',
-  onDelete: 'CASCADE'
 });
 
-Product.belongsTo(Category, {
-  foreignKey: 'category_id'
-});
 
 ProductTag.belongsTo(Product, {
   foreignKey: 'product_id'
